64bf189af7e062c080c66e2b449056dfbdb99e68
eric8810 <ericted8810@gmail.com>
2026-08-12 20:45:42 +0800

    ci: assert Electron 41 Windows toolchain

---
+++ script/dimcode/assert-windows-toolchain.ps1
@@ -0,0 +1,19 @@
+Set-StrictMode -Version Latest
+$ErrorActionPreference = 'Stop'
+
+$programFilesX86 = [Environment]::GetFolderPath('ProgramFilesX86')
+$vswhere = Join-Path $programFilesX86 'Microsoft Visual Studio\Installer\vswhere.exe'
+if (-not (Test-Path $vswhere)) {
+  throw "vswhere.exe is required at $vswhere"
+}
+
+$version = & $vswhere `
+  -latest `
+  -products '*' `
+  -requires Microsoft.VisualStudio.Component.VC.Tools.x86.x64 `
+  -property installationVersion
+if ($LASTEXITCODE -ne 0 -or $version -notmatch '^17\.') {
+  throw "Electron 41 requires Visual Studio 2022/17.x; detected: $version"
+}
+
+Write-Host "Visual Studio toolchain: $version"
